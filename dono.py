538abeb239fe7bbf21fa604b9e939beca3258b31
Junior Rosa <joel.junior@eitisolucoes.com.br>
2025-03-30 22:59:06 -0300

    refactor(new feats): adicionado alguns metodos de poo para performace

--- dono.py
+++ dono.py
@@ -1,9 +1,17 @@
 class Dono:
-    def __init__(self, nome: str, telefone: str, endereco: str, lista_de_pets: list = None):
+    def __init__(self, nome: str, telefone: str, endereco: str, lista_de_pets: list=[]):
         self.nome = nome
         self.telefone = telefone
         self.endereco = endereco
-        self.lista_de_pets = lista_de_pets if lista_de_pets is not None else []
+        self.lista_de_pets = lista_de_pets 
 
     def adicionar_pet(self, pet):
+        from pet import Pet # importado dentro da função para evitar dependência circular
+        
+        if not isinstance(pet, Pet):
+            raise ValueError("O objeto deve ser da classe Pet.")
+        
+        if pet in self.lista_de_pets:
+            raise ValueError("Este pet já está na lista.")
+        
         self.lista_de_pets.append(pet)
--- servico.py
+++ servico.py
@@ -1,15 +1,20 @@
 class Servico:
     def __init__(self, nome_do_servico, preco, duracao_estimada):
         self.nome_do_servico = nome_do_servico
-        self.preco = preco
+        self._preco = preco
         self.duracao_estimada = duracao_estimada
     
-    def exibir_detalhes(servico):
-        return [
-            f'Serviço: {servico.nome_do_servico}',
-            f'Preço: {servico.preco}',
-            f'Duração estimada: {servico.duracao_estimada}'
-        ]
+    def __str__(self):
+        return f"""'Serviço: {self.nome_do_servico}',
+'Preço: {self.preco}',
+'Duração estimada: {self.duracao_estimada}'"""
     
-    def atualizar_preco(self, preco):
-        self.preco = preco
+    @property
+    def preco(self):
+        return self._preco
+    
+    @preco.setter
+    def preco(self, preco):
+        if preco < 0:
+            raise ValueError("O preço não pode ser negativo.")
+        self._preco = preco
