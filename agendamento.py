aa95f0dfd5b526e64d323e03cd79658f3ed66a76
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2025-04-07 20:43:52 -0300

    Update agendamento.py

--- agendamento.py
+++ agendamento.py
@@ -11,19 +11,19 @@ class Agendamento:
         self.data = data
         self.horario = horario
     
-    def exibir_agendamento(agendamento):
+    def exibir_agendamento(self):
         return [
-            f'Nome do pet: {agendamento.pet.nome}',
-            f'Espécie do pet: {agendamento.pet.especie}',
-            f'Raça do {agendamento.pet.especie}: {agendamento.pet.raca}',
-            f'Idade: {agendamento.pet.idade} anos',
-            f'Nome do Dono: {agendamento.dono.nome}',
-            f'Telefone: {agendamento.dono.telefone}',
-            f'Endereço: {agendamento.dono.endereco}',
-            f'Serviço: {agendamento.servico.nome_do_servico}',
-            f'Preço: {agendamento.servico.preco}',
-            f'Duração estimada: {agendamento.servico.duracao_estimada}',
-            f'Data e hora: {agendamento.data} | {agendamento.horario}'
+            f'Nome do pet: {self.pet.nome}',
+            f'Espécie do pet: {self.pet.especie}',
+            f'Raça do {self.pet.especie}: {self.pet.raca}',
+            f'Idade: {self.pet.idade} anos',
+            f'Nome do Dono: {self.dono.nome}',
+            f'Telefone: {self.dono.telefone}',
+            f'Endereço: {self.dono.endereco}',
+            f'Serviço: {self.servico.nome_do_servico}',
+            f'Preço: {self.servico.preco}',
+            f'Duração estimada: {self.servico.duracao_estimada}',
+            f'Data e hora: {self.data} | {self.horario}'
         ]
 
 dono1 = Dono('Carlos', '99999-9999', 'Rua A, 123')
@@ -34,4 +34,4 @@ agendamento1 = Agendamento(pet1, dono1, servico1, '31/03/2025', '10:00')
 dono1.adicionar_pet(pet1)
 pet1.atualizar_dados(idade = 4)
 
-print(Agendamento.exibir_agendamento(agendamento1))
+print(agendamento1.exibir_agendamento())
